95c3e50a3b490fe08c6d2cee1aee0085e17a395c
jidelson <58210030+jidelson@users.noreply.github.com>
2020-06-19 09:56:13 -0700

    first commit

---
+++ client/src/components/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import React from 'react';
+
+export const axiosWithAuth = () => {
+    const token = window.localStorage.getItem('token');
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: 'http://localhost:5000'
+    });
+};
